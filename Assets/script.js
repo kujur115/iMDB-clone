1e4ae7776a58fa73ba10f0f45148535bec6cfcb9
Pratik Kujur <kujurpratik115@gmail.com>
2023-06-06 23:34:14 +0530

    autocomplete

--- Assets/script.js
+++ Assets/script.js
@@ -1,57 +1,6 @@
-const search = document.getElementById("search");
 const searchBox = document.getElementById("searchBox");
 const movieDetail = document.getElementById("movie-detail-container");
 let movieList = document.getElementsByClassName("movieList");
-
-search.addEventListener("keyup", getMovie);
-
-async function getMovie() {
-  const movie = search.value;
-  const url = `http://www.omdbapi.com/?apikey=dd8897cf&s=${movie}`;
-
-  while (document.getElementsByClassName("autocomplete")[0]) {
-    document.getElementsByClassName("autocomplete")[0].remove();
-  }
-
-  const response = await fetch(url);
-
-  const data = await response.json();
-  // console.log(data);
-  if (data.Response == "True") {
-    addToAutocomplete(data.Search);
-  }
-}
-
-function addToAutocomplete(results) {
-  let autoComplete = document.createElement("div");
-  autoComplete.setAttribute("class", "autocomplete");
-
-  autoComplete.innerHTML = ` <hr />
-  <ul class="autocomplete-list">
-  </ul>
-  `;
-  searchBox.appendChild(autoComplete);
-  let list = document.getElementsByClassName("autocomplete-list")[0];
-
-  for (let i = 0; i < results.length; i++) {
-    let li = document.createElement("li");
-    li.setAttribute("data-imdbId", `${results[i].imdbID}`);
-    li.setAttribute("class", "movieList");
-    li.innerHTML = `<a href="./movie.html?ref=${results[i].imdbID}">
-    <p>
-        <span class="list-title">${results[i].Title}</span>
-        <span class="list-year">${results[i].Year}</span>
-      </p>
-
-      <img
-        class="list-poster"
-        src='${results[i].Poster}'
-        alt="${results[i].Title}"
-      />
-      </a>`;
-    list.appendChild(li);
-  }
-}
 
 const latestMovies = [
   {
